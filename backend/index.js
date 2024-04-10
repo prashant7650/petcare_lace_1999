acf01157a8f771618cec2fd8e6eb1bdf8d708684
prashant7650 <prashantkad240999@gmail.com>
2024-04-10 10:58:52 +0530

    complete connection

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const {connection}=require("./db")
+const app=express()
+
+app.use(express.json())
+ 
+app.get("/",(req,res)=>{
+    res.send("welcome to the petcare website")
+})
+
+app.listen(8500, async(req,res)=>{
+    try{
+        await connection
+        console.log("connected to the database")
+        console.log("port is running on server 8500")
+    }catch(err){
+        console.log(err)
+        console.log("cannot connected")
+    }
+    console.log("port is running on server 8500")
+})
